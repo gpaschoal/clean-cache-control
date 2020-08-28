d827c0dda351994188efa571251e6db0dd0b4d4f
Guilherme <guilherme@guilherme-STI.(none)>
2020-08-27 22:20:40 -0300

    test: ensure AccountMongoRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -58,9 +58,18 @@ describe('Account Mongo Repository', () => {
     expect(account).toBeFalsy()
   })
 
-  test('Should return null if loadByEmail fails', async () => {
+  test('Should update the account accessToken on updateAccessToken success', async () => {
     const sut = makeSut()
-    const account = await sut.loadByEmail('any_email@mail.com')
-    expect(account).toBeFalsy()
+    const res = await accountCollection.insertOne({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+    const fakeAccount = res.ops[0]
+    expect(fakeAccount.accessToken).toBeFalsy()
+    await sut.updateAccessToken(fakeAccount._id, 'any_token')
+    const account = await accountCollection.findOne({ _id: fakeAccount._id })
+    expect(account).toBeTruthy()
+    expect(account.accessToken).toBe('any_token')
   })
 })
